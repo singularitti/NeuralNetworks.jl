d59401f0da1d41fdb29aacda2e367ab65bfac32d
singularitti <singularitti@outlook.com>
2023-06-02 23:27:42 -0400

    Use `Activation` in `Backpropagator`, deprecate fields `f` and `f′`

--- src/backpropagator.jl
+++ src/backpropagator.jl
@@ -1,25 +1,25 @@
 export Backpropagator
 
-struct Backpropagator{F,F′}
+struct Backpropagator
     network::Network
-    f::F
-    f′::F′
+    activation::Activation
 end
 
 (back::Backpropagator)(example::Example) = back(example.x, example.y)
 function (back::Backpropagator)(𝘅, 𝘆)
+    f, f′ = instantiate(back.activation)
     # Feed forward
     zs, activations = Vector{Float64}[], Vector{Float64}[𝘅]
     𝗮 = 𝘅
     for (_, wˡ, 𝗯ˡ) in excludeinput(eachlayer(back.network))
         𝘇ˡ = wˡ * 𝗮 .+ 𝗯ˡ
         push!(zs, 𝘇ˡ)
-        𝗮 = back.f.(𝘇ˡ)
+        𝗮 = f.(𝘇ˡ)
         push!(activations, 𝗮)
     end
     𝘇ᴸ, 𝗮ᴸ = zs[end], activations[end]
     # Backward pass
-    𝝳 = (𝗮ᴸ .- 𝘆) .* back.f′.(𝘇ᴸ)  # 𝝳ᴸ
+    𝝳 = (𝗮ᴸ .- 𝘆) .* f′.(𝘇ᴸ)  # 𝝳ᴸ
     𝝯w, 𝝯𝗯 = [kron(𝝳, activations[end - 1]')], [𝝳]  # 𝝯wᴸ, 𝝯𝗯ᴸ
     # Select `network` from layer L to 3, `zs` from layer L-1 to 2, `activations` from layer L-2 to 1
     for ((_, wˡ⁺¹, _), 𝘇ˡ, 𝗮ˡ⁻¹) in zip(
@@ -27,7 +27,7 @@ function (back::Backpropagator)(𝘅, 𝘆)
         zs[(end - 1):-1:begin],
         activations[(end - 2):-1:begin],
     )
-        𝝳 = transpose(wˡ⁺¹) * 𝝳 .* back.f′.(𝘇ˡ)
+        𝝳 = transpose(wˡ⁺¹) * 𝝳 .* f′.(𝘇ˡ)
         push!(𝝯w, kron(𝝳, 𝗮ˡ⁻¹'))
         push!(𝝯𝗯, 𝝳)
     end
