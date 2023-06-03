6752bd868f9c3e32c766fc5ed323ba5f67b7052c
singularitti <singularitti@outlook.com>
2023-06-02 23:42:28 -0400

    Add `functionof` & `derivativeof` to `SigmoidActivation` in src/activation.jl

--- src/activation.jl
+++ src/activation.jl
@@ -1,4 +1,10 @@
-export LinearActivation, SigmoidActivation, ReLUActivation, LogisticActivation, instantiate
+export LinearActivation,
+    SigmoidActivation,
+    ReLUActivation,
+    LogisticActivation,
+    instantiate,
+    functionof,
+    derivativeof
 
 abstract type Activation end
 struct LinearActivation <: Activation end
@@ -6,8 +12,15 @@ struct SigmoidActivation <: Activation end
 struct ReLUActivation <: Activation end
 const LogisticActivation = SigmoidActivation
 
-function instantiate(::SigmoidActivation)
+instantiate(activation::Activation) = (functionof(activation), derivativeof(activation))
+
+function functionof(::SigmoidActivation)
     sigmoid(z) = 1 / (1 + exp(-z))
+    return sigmoid
+end
+
+function derivativeof(::SigmoidActivation)
+    sigmoid = functionof(SigmoidActivation())
     sigmoid′(z) = sigmoid(z) * (1 - sigmoid(z))
-    return (sigmoid, sigmoid′)
+    return sigmoid′
 end
