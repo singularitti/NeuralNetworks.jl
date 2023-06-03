e05b9a955aa2951b71de8bbab31af4320efb7397
singularitti <singularitti@outlook.com>
2023-06-03 03:32:05 -0400

    * feat(activation.jl): add HyperbolicTangent activation function and alias TanhActivation to it

--- src/activation.jl
+++ src/activation.jl
@@ -1,6 +1,7 @@
 export LinearActivation,
     SigmoidActivation,
     ReLUActivation,
+    HyperbolicTangent,
     LogisticActivation,
     instantiate,
     functionof,
@@ -10,18 +11,19 @@ abstract type Activation end
 struct LinearActivation <: Activation end
 struct SigmoidActivation <: Activation end
 struct ReLUActivation <: Activation end
-struct TanhActivation <: Activation end
+struct HyperbolicTangent <: Activation end
+const TanhActivation = HyperbolicTangent
 const LogisticActivation = SigmoidActivation
 
 instantiate(activation::Activation) = (functionof(activation), derivativeof(activation))
 
 functionof(::SigmoidActivation) = z -> 1 / (1 + exp(-z))
-functionof(::TanhActivation) = tanh
+functionof(::HyperbolicTangent) = tanh
 functionof(::ReLUActivation) = z -> z >= 0 ? z : 0
 
 function derivativeof(::SigmoidActivation)
     sigmoid = functionof(SigmoidActivation())
     return z -> sigmoid(z) * (1 - sigmoid(z))
 end
-derivativeof(::TanhActivation) = z -> 1 - tanh(z)^2
+derivativeof(::HyperbolicTangent) = z -> 1 - tanh(z)^2
 derivativeof(::ReLUActivation) = z -> z >= 0 ? 1 : 0
