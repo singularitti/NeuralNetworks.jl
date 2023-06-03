889d88c67c1be47ddd60aa914685d467a9d6670a
singularitti <singularitti@outlook.com>
2023-06-02 23:57:05 -0400

    Add `TanhActivation` & more `functionof` & `derivativeof`

--- src/activation.jl
+++ src/activation.jl
@@ -10,17 +10,18 @@ abstract type Activation end
 struct LinearActivation <: Activation end
 struct SigmoidActivation <: Activation end
 struct ReLUActivation <: Activation end
+struct TanhActivation <: Activation end
 const LogisticActivation = SigmoidActivation
 
 instantiate(activation::Activation) = (functionof(activation), derivativeof(activation))
 
-function functionof(::SigmoidActivation)
-    sigmoid(z) = 1 / (1 + exp(-z))
-    return sigmoid
-end
+functionof(::SigmoidActivation) = z -> 1 / (1 + exp(-z))
+functionof(::TanhActivation) = tanh
+functionof(::ReLUActivation) = z -> z >= 0 ? z : 0
 
 function derivativeof(::SigmoidActivation)
     sigmoid = functionof(SigmoidActivation())
-    sigmoid′(z) = sigmoid(z) * (1 - sigmoid(z))
-    return sigmoid′
+    return z -> sigmoid(z) * (1 - sigmoid(z))
 end
+derivativeof(::TanhActivation) = z -> 1 - tanh(z)^2
+derivativeof(::ReLUActivation) = z -> z >= 0 ? 1 : 0
