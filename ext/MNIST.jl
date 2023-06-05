7a3381604e72ecfb02f09dab5c5429e03cb10237
singularitti <singularitti@outlook.com>
2023-06-05 00:29:59 -0400

    Add ext/MNIST.jl

--- ext/MNIST.jl
+++ ext/MNIST.jl
@@ -1,5 +1,16 @@
 module MNIST
 
+using MLDatasets: MLDatasets
+using NeuralNetworks: Example
 
-    
+export loaddata
+
+function loaddata(type=:train)
+    @assert type ∈ (:train, :test)
+    𝗫, 𝘆 = MLDatasets.MNIST(type)[:]
+    𝗫 = (vec(X) for X in eachslice(𝗫; dims=3))  # Each original data is 28x28 matrices
+    𝘆 = ([index == y for index in 0:9] for y in 𝘆)  # Each original data is a number
+    return collect(Example(X, y) for (X, y) in zip(𝗫, 𝘆))
+end
+
 end
