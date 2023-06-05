e7f34334b7ab90c4e6d6acf569e1001aa2cf9cae
singularitti <singularitti@outlook.com>
2023-06-05 00:30:38 -0400

    Fix constructor of `Example`

--- src/example.jl
+++ src/example.jl
@@ -4,5 +4,6 @@ struct Example{X,Y}
     x::Vector{X}
     y::Vector{Y}
 end
+Example(x::AbstractVector{X}, y::AbstractVector{Y}) where {X,Y} = Example{X,Y}(x, y)
 
 unwrap(example::Example) = example.x, example.y
