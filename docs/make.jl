9c276ddf21219d0964079b28209a1da0ac817d31
singularitti <singularitti@outlook.com>
2023-11-08 02:39:38 -0500

    Fix docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -13,9 +13,27 @@ makedocs(;
         canonical="https://singularitti.github.io/NeuralNetworks.jl",
         edit_link="main",
         assets=String[],
+        mathengine=Documenter.HTMLWriter.MathJax3(),
     ),
     pages=[
         "Home" => "index.md",
+        "Manual" => [
+            "Installation Guide" => "man/installation.md",
+            "Backpropagation" => "man/backpropagation.md",
+            "Troubleshooting" => "man/troubleshooting.md",
+        ],
+        "Reference" => Any[
+            "Public API" => "lib/public.md",
+            # "Internals" => map(
+            #     s -> "lib/internals/$(s)",
+            #     sort(readdir(joinpath(@__DIR__, "src/lib/internals")))
+            # ),
+        ],
+        "Developer Docs" => [
+            "Contributing" => "developers/contributing.md",
+            "Style Guide" => "developers/style-guide.md",
+            "Design Principles" => "developers/design-principles.md",
+        ],
     ],
 )
 
