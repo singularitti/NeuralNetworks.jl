d1fad43c65e2ec19f40958df0071b9d0b3bdc6e6
singularitti <singularitti@outlook.com>
2024-07-29 17:05:01 -0600

    Redefine `MultilayerPerceptron`, put `activations` into its fields

--- src/network.jl
+++ src/network.jl
@@ -1,20 +1,21 @@
 using ComputedFieldTypes: @computed
 
 export MultilayerPerceptron, feedforward
-
-@computed struct MultilayerPerceptron{N}
+@computed struct MultilayerPerceptron{T,N}
     layers::NTuple{N,Int64}
     weights::NTuple{N - 1,Matrix{Float64}}
     biases::NTuple{N - 1,Vector{Float64}}
+    activations::NTuple{N - 1,T}
 end
-function MultilayerPerceptron(layers)
+function MultilayerPerceptron(layers, activations)
     weights = Tuple(
         zeros(nⱼ, nₖ) for (nⱼ, nₖ) in zip(layers[(begin + 1):end], layers[begin:(end - 1)])
     )  # Do not use `undef` here, as it will be added and subtracted from!
     biases = Tuple(zeros(nⱼ) for nⱼ in layers[(begin + 1):end])  # Do not use `undef` here!
-    return MultilayerPerceptron{length(layers)}(layers, weights, biases)
+    return MultilayerPerceptron{Base.promote_typeof(activations...),length(layers)}(
+        Tuple(layers), weights, biases, Tuple(activations)
+    )
 end
-MultilayerPerceptron(layers::Integer...) = MultilayerPerceptron(layers)
 
 (network::MultilayerPerceptron)(f, 𝘅) = feedforward(f, network.weights, network.biases, 𝘅)
 
